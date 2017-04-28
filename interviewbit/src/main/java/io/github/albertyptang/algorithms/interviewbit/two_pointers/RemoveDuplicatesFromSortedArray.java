2812066d66a9b52014457f21c6e57a0c3120b086
aytang <albertyptang@gmail.com>
2017-04-27 23:27:42 -0700

    Three Sum Zero Remove Duplicates From Sorted Array

---
+++ interviewbit/src/main/java/io/github/albertyptang/algorithms/interviewbit/two_pointers/RemoveDuplicatesFromSortedArray.java
@@ -0,0 +1,29 @@
+package io.github.albertyptang.algorithms.interviewbit.two_pointers;
+
+import java.util.ArrayList;
+
+/**
+ * Remove duplicates from Sorted Array
+ * Given a sorted array, remove the duplicates in place such that each element appears only once and return the new length.
+ *
+ * Note that even though we want you to return the new length, make sure to change the original array as well in place
+ *
+ * Do not allocate extra space for another array, you must do this in place with constant memory.
+ *
+ * Example:
+ * Given input array A = [1,1,2],
+ * Your function should return length = 2, and A is now [1,2].
+ * @author atang
+ */
+public class RemoveDuplicatesFromSortedArray {
+    public static int removeDuplicates(ArrayList<Integer> a) {
+        int i = a.size() - 1;
+        while (i > 0) {
+            if (a.get(i).equals(a.get(i - 1))) {
+                a.remove(i);
+            }
+            i--;
+        }
+        return a.size();
+    }
+}
---
+++ interviewbit/src/test/java/io/github/albertyptang/algorithms/interviewbit/two_pointers/ThreeSumZeroTest.java
@@ -0,0 +1,23 @@
+package io.github.albertyptang.algorithms.interviewbit.two_pointers;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author atang
+ */
+public class ThreeSumZeroTest {
+    @Test
+    public void threeSum() throws Exception {
+
+        final ArrayList<Integer> a = new ArrayList<>(Arrays.asList(-1, 0, 1, 2, -1, -4));
+        assertEquals(2, ThreeSumZero.threeSum(a).size());
+
+        final ArrayList<Integer> b = new ArrayList<>(Arrays.asList(1, -4, 0, 0, 5, -5, 1, 0, -2, 4, -4, 1, -1, -4, 3, 4, -1, -1, -3));
+        assertEquals(12, ThreeSumZero.threeSum(b).size());
+    }
+}
